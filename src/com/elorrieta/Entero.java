7519f7a075e20172c4ce9b90deb37fa96f830649
Ana <Ana@gmail.com>
2019-12-09 18:57:31 +0100

    modificacion en entero.java

--- src/com/elorrieta/Entero.java
+++ src/com/elorrieta/Entero.java
@@ -1,5 +1,8 @@
 package com.elorrieta;
 
 public class Entero {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inicio de proyecto");
+	}
 }
